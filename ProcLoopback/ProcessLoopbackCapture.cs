7698766e3530baf1b3221166e83fac8db73e8826
José Luis <21303541+joseluisct@users.noreply.github.com>
2024-10-31 21:15:32 +0100

    Se añade código C# generado por Copilot

--- ProcLoopback/ProcessLoopbackCapture.cs
+++ ProcLoopback/ProcessLoopbackCapture.cs
@@ -1,60 +1,452 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Windows.Win32.Foundation;
-using Windows.Win32.Media.Audio;
-using Windows.Win32.System.Com;
-//using static Windows.Win32.PInvoke;
 
-namespace ProcLoopback;
+public enum eCaptureState
+{
+    READY = 0,
+    CAPTURING,
+    PAUSED
+}
+
+public enum eCaptureError
+{
+    NONE = 0,
+    PARAM,
+    STATE,
+    NOT_AVAILABLE,
+    FORMAT,
+    PROCESSID,
+    DEVICE,
+    ACTIVATION,
+    INITIALIZE,
+    SERVICE,
+    START,
+    STOP,
+    EVENT,
+    INTERFACE
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct WAVEFORMATEX
+{
+    public ushort wFormatTag;
+    public ushort nChannels;
+    public uint nSamplesPerSec;
+    public uint nAvgBytesPerSec;
+    public ushort nBlockAlign;
+    public ushort wBitsPerSample;
+    public ushort cbSize;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct AUDIOCLIENT_ACTIVATION_PARAMS
+{
+    public int ActivationType;
+    public ProcessLoopbackParams ProcessLoopbackParams;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct ProcessLoopbackParams
+{
+    public int ProcessLoopbackMode;
+    public uint TargetProcessId;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct PROPVARIANT
+{
+    public ushort vt;
+    public ushort wReserved1;
+    public ushort wReserved2;
+    public ushort wReserved3;
+    public IntPtr blob;
+    public uint cbSize;
+}
+
+[ComImport]
+[Guid("1CB9AD4C-DBFA-4c32-B178-C2F568A703B2")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IAudioClient
+{
+    int Initialize(uint ShareMode, uint StreamFlags, long hnsBufferDuration, long hnsPeriodicity, ref WAVEFORMATEX pFormat, IntPtr AudioSessionGuid);
+    int GetService(ref Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object ppv);
+    int Start();
+    int Stop();
+    int Reset();
+    int SetEventHandle(IntPtr eventHandle);
+}
+
+[ComImport]
+[Guid("C8ADBD64-E71E-48a0-A4DE-185C395CD317")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IAudioCaptureClient
+{
+    int GetBuffer(out IntPtr ppData, out uint pNumFramesToRead, out uint pdwFlags, out ulong pu64DevicePosition, out ulong pu64QPCPosition);
+    int ReleaseBuffer(uint NumFramesRead);
+}
+
+[ComImport]
+[Guid("41D949AB-9862-444A-80F6-C261334DA5EB")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IActivateAudioInterfaceAsyncOperation
+{
+    int GetActivateResult(out int activateResult, [MarshalAs(UnmanagedType.IUnknown)] out object activatedInterface);
+    void Release();
+}
+
+[ComImport]
+[Guid("41D949AB-9862-444A-80F6-C261334DA5EB")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IActivateAudioInterfaceCompletionHandler
+{
+    int ActivateCompleted(IActivateAudioInterfaceAsyncOperation activateOperation);
+}
 
 public class ActivateAudioInterfaceAsyncCallback : IActivateAudioInterfaceCompletionHandler
 {
-    private ManualResetEvent m_bActivateCompleted = new ManualResetEvent(false);
-    public ActivateAudioInterfaceAsyncCallback()
-    {
-    }
+    private AutoResetEvent activateCompletedEvent = new AutoResetEvent(false);
 
     public void Wait()
     {
-        m_bActivateCompleted.WaitOne();
-        m_bActivateCompleted.Reset();
+        activateCompletedEvent.WaitOne();
     }
 
-    void ActivateCompleted(IActivateAudioInterfaceAsyncOperation activateOperation)
+    public int ActivateCompleted(IActivateAudioInterfaceAsyncOperation activateOperation)
     {
-        m_bActivateCompleted.Set();
-    }
-
-    public void QueryInterface(ref Guid riid, out IntPtr ppvObject)
-    {
-        if (riid == typeof(IAgileObject).GUID)
-        {
-            ppvObject = Marshal.GetComInterfaceForObject(this, typeof(IActivateAudioInterfaceCompletionHandler));
-        }
-        else
-        {
-            ppvObject = IntPtr.Zero;
-            throw new COMException("No such interface supported", (int)HRESULT.E_NOINTERFACE);
-        }
-    }
-
-    public uint AddRef()
-    {
-        return 1;
-    }
-
-    public uint Release()
-    {
-        return 0;
-    }
-
-    void IActivateAudioInterfaceCompletionHandler.ActivateCompleted(IActivateAudioInterfaceAsyncOperation activateOperation)
-    {
-        throw new NotImplementedException();
+        activateCompletedEvent.Set();
+        return 0; // S_OK
     }
 }
 
 public class ProcessLoopbackCapture
 {
+    private IAudioClient audioClient;
+    private IAudioCaptureClient audioCaptureClient;
+    private IntPtr sampleReadyEvent;
+    private WAVEFORMATEX captureFormat;
+    private eCaptureState captureState;
+    private Thread mainAudioThread;
+    private bool runAudioThreads;
+    private int lastError;
+    private bool captureFormatInitialized;
+    private uint processId;
+    private bool processInclusive;
+    private Action<List<byte>, List<byte>, object> callbackFunc;
+    private object callbackFuncUserData;
+    private uint callbackInterval;
+    private List<byte> audioData;
+    private double maxExecutionTime;
+    private uint mainThreadBytesToSkip;
+
+    public ProcessLoopbackCapture()
+    {
+        captureState = eCaptureState.READY;
+        runAudioThreads = false;
+        audioData = new List<byte>();
+        maxExecutionTime = 0.0;
+        mainThreadBytesToSkip = 0;
+    }
+
+    public eCaptureError SetCaptureFormat(uint sampleRate, ushort bitDepth, ushort channelCount, ushort formatTag = 1)
+    {
+        if (captureState != eCaptureState.READY) return eCaptureError.STATE;
+        if (sampleRate < 1000) return eCaptureError.PARAM;
+        if (bitDepth == 0 || bitDepth > 32 || (bitDepth % 8) != 0) return eCaptureError.PARAM;
+        if (channelCount < 1 || channelCount > 1024) return eCaptureError.PARAM;
+
+        captureFormat = new WAVEFORMATEX
+        {
+            wFormatTag = formatTag,
+            nChannels = channelCount,
+            nSamplesPerSec = sampleRate,
+            wBitsPerSample = bitDepth,
+            nBlockAlign = (ushort)(bitDepth / 8 * channelCount),
+            nAvgBytesPerSec = sampleRate * (uint)(bitDepth / 8 * channelCount)
+        };
+
+        captureFormatInitialized = true;
+        return eCaptureError.NONE;
+    }
+
+    public bool CopyCaptureFormat(out WAVEFORMATEX format)
+    {
+        if (!captureFormatInitialized)
+        {
+            format = new WAVEFORMATEX();
+            return false;
+        }
+
+        format = captureFormat;
+        return true;
+    }
+
+    public eCaptureError SetTargetProcess(uint processId, bool inclusive = true)
+    {
+        if (captureState != eCaptureState.READY) return eCaptureError.STATE;
+        if (processId == 0) return eCaptureError.PARAM;
+
+        this.processId = processId;
+        this.processInclusive = inclusive;
+        return eCaptureError.NONE;
+    }
+
+    public eCaptureError SetCallback(Action<List<byte>, List<byte>, object> callbackFunc, object userData = null)
+    {
+        if (captureState != eCaptureState.READY) return eCaptureError.STATE;
+
+        this.callbackFunc = callbackFunc;
+        this.callbackFuncUserData = userData;
+        return eCaptureError.NONE;
+    }
+
+    public eCaptureError SetCallbackInterval(uint interval)
+    {
+        if (captureState != eCaptureState.READY) return eCaptureError.STATE;
+
+        callbackInterval = interval < 1 ? 1 : interval;
+        return eCaptureError.NONE;
+    }
+
+    public eCaptureState GetState()
+    {
+        return captureState;
+    }
+
+    public eCaptureError StartCapture()
+    {
+        if (captureState != eCaptureState.READY) return eCaptureError.STATE;
+        if (!captureFormatInitialized) return eCaptureError.FORMAT;
+        if (processId == 0) return eCaptureError.PROCESSID;
+
+        AUDIOCLIENT_ACTIVATION_PARAMS activationParams = new AUDIOCLIENT_ACTIVATION_PARAMS
+        {
+            ActivationType = 1, // AUDIOCLIENT_ACTIVATION_TYPE_PROCESS_LOOPBACK
+            ProcessLoopbackParams = new ProcessLoopbackParams
+            {
+                ProcessLoopbackMode = processInclusive ? 1 : 0, // PROCESS_LOOPBACK_MODE_INCLUDE_TARGET_PROCESS_TREE or PROCESS_LOOPBACK_MODE_EXCLUDE_TARGET_PROCESS_TREE
+                TargetProcessId = processId
+            }
+        };
+
+        PROPVARIANT propVariant = new PROPVARIANT
+        {
+            vt = 65, // VT_BLOB
+            cbSize = (uint)Marshal.SizeOf(activationParams),
+            blob = Marshal.AllocHGlobal(Marshal.SizeOf(activationParams))
+        };
+        Marshal.StructureToPtr(activationParams, propVariant.blob, false);
+
+        IActivateAudioInterfaceAsyncOperation activationOperation = null;
+        ActivateAudioInterfaceAsyncCallback asyncCallback = new ActivateAudioInterfaceAsyncCallback();
+        lastError = ActivateAudioInterfaceAsync("VIRTUAL_AUDIO_DEVICE_PROCESS_LOOPBACK", typeof(IAudioClient).GUID, ref propVariant, asyncCallback, out activationOperation);
+        if (lastError != 0)
+        {
+            Reset();
+            return eCaptureError.DEVICE;
+        }
+
+        asyncCallback.Wait();
+        lastError = activationOperation.GetActivateResult(out lastError, out object activatedInterface);
+        activationOperation.Release();
+        if (lastError != 0)
+        {
+            Reset();
+            return eCaptureError.ACTIVATION;
+        }
+
+        audioClient = (IAudioClient)activatedInterface;
+        lastError = audioClient.Initialize(1, 0x00020000 | 0x00040000 | 0x08000000 | 0x10000000, 0, 0, ref captureFormat, IntPtr.Zero);
+        if (lastError != 0)
+        {
+            Reset();
+            return eCaptureError.INITIALIZE;
+        }
+
+        Guid IID_IAudioCaptureClient = typeof(IAudioCaptureClient).GUID;
+        lastError = audioClient.GetService(ref IID_IAudioCaptureClient, out object audioCaptureClientObj);
+        if (lastError != 0)
+        {
+            Reset();
+            return eCaptureError.SERVICE;
+        }
+        audioCaptureClient = (IAudioCaptureClient)audioCaptureClientObj;
+
+        sampleReadyEvent = CreateEvent(IntPtr.Zero, false, false, null);
+        lastError = audioClient.SetEventHandle(sampleReadyEvent);
+        if (lastError != 0 || sampleReadyEvent == IntPtr.Zero)
+        {
+            Reset();
+            return eCaptureError.EVENT;
+        }
+
+        lastError = audioClient.Start();
+        if (lastError != 0)
+        {
+            Reset();
+            return eCaptureError.START;
+        }
+
+        StartThreads(0.0);
+        captureState = eCaptureState.CAPTURING;
+        return eCaptureError.NONE;
+    }
+
+    public eCaptureError StopCapture()
+    {
+        if (captureState == eCaptureState.READY) return eCaptureError.STATE;
+        Reset();
+        return eCaptureError.NONE;
+    }
+
+    public eCaptureError PauseCapture()
+    {
+        if (captureState != eCaptureState.CAPTURING) return eCaptureError.STATE;
+        captureState = eCaptureState.PAUSED;
+        lastError = audioClient.Stop();
+        if (lastError != 0) return eCaptureError.STOP;
+        StopThreads();
+        return eCaptureError.NONE;
+    }
+
+    public eCaptureError ResumeCapture(double initialDurationToSkip = 0.1)
+    {
+        if (captureState != eCaptureState.PAUSED) return eCaptureError.STATE;
+        captureState = eCaptureState.CAPTURING;
+        ResetEvent(sampleReadyEvent);
+        lastError = audioClient.Start();
+        if (lastError != 0) return eCaptureError.START;
+        StartThreads(initialDurationToSkip);
+        return eCaptureError.NONE;
+    }
+
+    public int GetLastErrorResult()
+    {
+        return lastError;
+    }
+
+    public double GetMaxExecutionTime()
+    {
+        return maxExecutionTime;
+    }
+
+    public void ResetMaxExecutionTime()
+    {
+        maxExecutionTime = 0.0;
+    }
+
+    public eCaptureError GetQueueSize(out int size)
+    {
+        size = 0;
+        return eCaptureError.NOT_AVAILABLE;
+    }
+
+    private void Reset()
+    {
+        StopThreads();
+        if (captureState == eCaptureState.CAPTURING)
+        {
+            audioClient.Stop();
+        }
+        if (audioCaptureClient != null)
+        {
+            Marshal.ReleaseComObject(audioCaptureClient);
+            audioCaptureClient = null;
+        }
+        if (audioClient != null)
+        {
+            audioClient.Reset();
+            Marshal.ReleaseComObject(audioClient);
+            audioClient = null;
+        }
+        if (sampleReadyEvent != IntPtr.Zero)
+        {
+            CloseHandle(sampleReadyEvent);
+            sampleReadyEvent = IntPtr.Zero;
+        }
+        captureState = eCaptureState.READY;
+    }
+
+    private void StartThreads(double initialDurationToSkip)
+    {
+        if (runAudioThreads) return;
+        if (initialDurationToSkip < 0.0) initialDurationToSkip = 0.0;
+        mainThreadBytesToSkip = (uint)(captureFormat.nSamplesPerSec * initialDurationToSkip) * captureFormat.nBlockAlign;
+        runAudioThreads = true;
+        mainAudioThread = new Thread(ProcessMainToCallback);
+        mainAudioThread.Start();
+    }
+
+    private void StopThreads()
+    {
+        if (!runAudioThreads) return;
+        runAudioThreads = false;
+        mainAudioThread.Join();
+        mainAudioThread = null;
+        audioData.Clear();
+    }
+
+    private void ProcessMainToCallback()
+    {
+        uint taskIndex = 0;
+        IntPtr taskHandle = AvSetMmThreadCharacteristics("Pro Audio", ref taskIndex);
+        IntPtr pData = IntPtr.Zero;
+        uint framesAvailable;
+        uint bytesAvailable;
+        uint captureFlags;
+        while (runAudioThreads)
+        {
+            if (WaitForSingleObject(sampleReadyEvent, 50) == 0)
+            {
+                if (!runAudioThreads) break;
+                var tickStart = DateTime.Now;
+                while (audioCaptureClient.GetBuffer(out pData, out framesAvailable, out captureFlags, out _, out _) == 0)
+                {
+                    bytesAvailable = framesAvailable * captureFormat.nBlockAlign;
+                    for (uint i = 0; i < bytesAvailable; ++i)
+                    {
+                        if (mainThreadBytesToSkip != 0)
+                        {
+                            --mainThreadBytesToSkip;
+                        }
+                        else
+                        {
+                            audioData.Add(Marshal.ReadByte(pData, (int)i));
+                        }
+                    }
+                    audioCaptureClient.ReleaseBuffer(framesAvailable);
+                }
+                if (audioData.Count > 0)
+                {
+                    callbackFunc?.Invoke(audioData, audioData, callbackFuncUserData);
+                    audioData.Clear();
+                }
+                var duration = (DateTime.Now - tickStart).TotalMilliseconds;
+                if (duration > maxExecutionTime) maxExecutionTime = duration;
+            }
+        }
+        if (taskHandle != IntPtr.Zero) AvRevertMmThreadCharacteristics(taskHandle);
+    }
+
+    [DllImport("api-ms-win-core-synch-l1-2-0.dll", SetLastError = true)]
+    private static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+
+    [DllImport("api-ms-win-core-com-l1-1-0.dll", SetLastError = true)]
+    private static extern int ActivateAudioInterfaceAsync(string deviceInterfacePath, Guid riid, ref PROPVARIANT activationParams, IActivateAudioInterfaceCompletionHandler completionHandler, out IActivateAudioInterfaceAsyncOperation activationOperation);
+
+    [DllImport("avrt.dll", SetLastError = true)]
+    private static extern IntPtr AvSetMmThreadCharacteristics(string taskName, ref uint taskIndex);
+
+    [DllImport("avrt.dll", SetLastError = true)]
+    private static extern bool AvRevertMmThreadCharacteristics(IntPtr taskHandle);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool CloseHandle(IntPtr hObject);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool ResetEvent(IntPtr hEvent);
 }
--- ProcLoopback/Program.cs
+++ ProcLoopback/Program.cs
@@ -1,8 +1,267 @@
-﻿namespace ProcLoopback;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 
 public class Program
 {
-    static void Main(string[] args)
+    private static ProcessLoopbackCapture g_LoopbackCapture = new ProcessLoopbackCapture();
+    private static object g_AudioDataLock = new object();
+    private static List<byte> g_Data = new List<byte>();
+
+    private const bool WRITE_RAW_FILE = false;
+    private const uint DEFAULT_SAMPLE_RATE = 44100U;
+    private const ushort DEFAULT_BIT_DEPTH = 16;
+    private const ushort DEFAULT_CHANNEL_COUNT = 2;
+
+    public static void Main(string[] args)
     {
+        if (CoInitializeEx(IntPtr.Zero, COINIT_MULTITHREADED) != 0)
+        {
+            Console.WriteLine("Failed to init COM");
+            Environment.Exit(1);
+        }
+
+        uint sampleRate = DEFAULT_SAMPLE_RATE;
+        ushort bitDepth = DEFAULT_BIT_DEPTH;
+        ushort channelCount = DEFAULT_CHANNEL_COUNT;
+
+        if (args.Length >= 1)
+        {
+            if (!uint.TryParse(args[0], out sampleRate))
+            {
+                sampleRate = DEFAULT_SAMPLE_RATE;
+            }
+        }
+
+        if (args.Length >= 2)
+        {
+            if (!ushort.TryParse(args[1], out bitDepth))
+            {
+                bitDepth = DEFAULT_BIT_DEPTH;
+            }
+        }
+
+        if (args.Length >= 3)
+        {
+            if (!ushort.TryParse(args[2], out channelCount))
+            {
+                channelCount = DEFAULT_CHANNEL_COUNT;
+            }
+        }
+
+        Console.WriteLine($"Sample Rate: {sampleRate}");
+        Console.WriteLine($"Bit Depth : {bitDepth}");
+        Console.WriteLine($"Channels  : {channelCount}");
+        Console.WriteLine();
+
+        bool runApplication = true;
+        while (runApplication)
+        {
+            g_Data.Clear();
+
+            uint processId = 0;
+            do
+            {
+                Console.WriteLine("Enter the Process Name to listen to (incl. .exe):");
+                string processName = Console.ReadLine();
+                if (string.IsNullOrEmpty(processName)) continue;
+
+                List<uint> processIds = FindParentProcessIDs(processName);
+                if (processIds.Count > 0) processId = processIds[0];
+            }
+            while (processId == 0);
+
+            Console.WriteLine($"PID: {processId}");
+
+            g_LoopbackCapture.SetCaptureFormat(sampleRate, bitDepth, channelCount, 1);
+            g_LoopbackCapture.SetTargetProcess(processId, true);
+            g_LoopbackCapture.SetCallback(OnDataCapture);
+            g_LoopbackCapture.SetCallbackInterval(40);
+
+            eCaptureError eError = g_LoopbackCapture.StartCapture();
+            if (eError != eCaptureError.NONE)
+            {
+                int hr = g_LoopbackCapture.GetLastErrorResult();
+                Console.WriteLine();
+                Console.WriteLine($"ERROR ({(int)eError}): {GetErrorText(eError)}");
+                Console.WriteLine($"HR: 0x{hr:X}");
+                Console.WriteLine($"HR Text: {new System.ComponentModel.Win32Exception(hr).Message}");
+                Console.WriteLine();
+                continue;
+            }
+
+            Console.WriteLine("Capturing audio.");
+            Console.WriteLine("Press Enter to stop and save.");
+            Console.WriteLine("Type \"discard\" to stop without saving.");
+            Console.WriteLine("Type \"pause\" to pause or resume capture.");
+            Console.WriteLine("Type \"hang\" to simulate a long hang in the callback.");
+            Console.WriteLine("Type \"exit\" to exit the application.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.Equals("discard", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                if (input.Equals("pause", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (g_LoopbackCapture.GetState() == eCaptureState.CAPTURING)
+                    {
+                        eCaptureError pauseError = g_LoopbackCapture.PauseCapture();
+                        if (pauseError != eCaptureError.NONE)
+                        {
+                            int hr = g_LoopbackCapture.GetLastErrorResult();
+                            Console.WriteLine();
+                            Console.WriteLine($"ERROR ({(int)pauseError}): {GetErrorText(pauseError)}");
+                            Console.WriteLine($"HR: 0x{hr:X}");
+                            Console.WriteLine($"HR Text: {new System.ComponentModel.Win32Exception(hr).Message}");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Capture paused");
+                        }
+                    }
+                    else if (g_LoopbackCapture.GetState() == eCaptureState.PAUSED)
+                    {
+                        eCaptureError resumeError = g_LoopbackCapture.ResumeCapture();
+                        if (resumeError != eCaptureError.NONE)
+                        {
+                            int hr = g_LoopbackCapture.GetLastErrorResult();
+                            Console.WriteLine();
+                            Console.WriteLine($"ERROR ({(int)resumeError}): {GetErrorText(resumeError)}");
+                            Console.WriteLine($"HR: 0x{hr:X}");
+                            Console.WriteLine($"HR Text: {new System.ComponentModel.Win32Exception(hr).Message}");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Capture resumed");
+                        }
+                    }
+                    continue;
+                }
+                if (input.Equals("hang", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Hanging Callback Thread for 10 seconds ...");
+                    lock (g_AudioDataLock)
+                    {
+                        Thread.Sleep(10000);
+                    }
+                    Console.WriteLine("Done.");
+                    continue;
+                }
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    runApplication = false;
+                    break;
+                }
+                if (string.IsNullOrEmpty(input))
+                {
+                    g_LoopbackCapture.StopCapture();
+                    string fileName = $"out-{Environment.TickCount}.wav";
+                    Console.WriteLine($"Saving Audio to \"{fileName}\" ...");
+                    g_LoopbackCapture.CopyCaptureFormat(out WAVEFORMATEX format);
+                    WriteWavFile(fileName, g_Data, format);
+                    Console.WriteLine("Done");
+                    break;
+                }
+            }
+            g_LoopbackCapture.StopCapture();
+        }
+
+        CoUninitialize();
     }
+
+    private static void OnDataCapture(List<byte> data, List<byte> _, object userData)
+    {
+        lock (g_AudioDataLock)
+        {
+            g_Data.AddRange(data);
+        }
+    }
+
+    private static void WriteWavFile(string fileName, List<byte> data, WAVEFORMATEX format)
+    {
+        using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+        {
+            using (BinaryWriter writer = new BinaryWriter(fs))
+            {
+                if (!WRITE_RAW_FILE)
+                {
+                    writer.Write(Encoding.ASCII.GetBytes("RIFF"));
+                    writer.Write(0); // Placeholder for file size
+                    writer.Write(Encoding.ASCII.GetBytes("WAVE"));
+                    writer.Write(Encoding.ASCII.GetBytes("fmt "));
+                    writer.Write(Marshal.SizeOf(format));
+                    writer.Write(format.wFormatTag);
+                    writer.Write(format.nChannels);
+                    writer.Write(format.nSamplesPerSec);
+                    writer.Write(format.nAvgBytesPerSec);
+                    writer.Write(format.nBlockAlign);
+                    writer.Write(format.wBitsPerSample);
+                    writer.Write(format.cbSize);
+                    writer.Write(Encoding.ASCII.GetBytes("data"));
+                    writer.Write(data.Count);
+                    writer.Write(data.ToArray());
+
+                    long fileSize = fs.Length;
+                    fs.Seek(4, SeekOrigin.Begin);
+                    writer.Write((int)(fileSize - 8));
+                    fs.Seek(40, SeekOrigin.Begin);
+                    writer.Write(data.Count);
+                }
+                else
+                {
+                    writer.Write(data.ToArray());
+                }
+            }
+        }
+    }
+
+    private static List<uint> FindParentProcessIDs(string executableName)
+    {
+        List<uint> processIdList = new List<uint>();
+        foreach (Process process in Process.GetProcessesByName(Path.GetFileNameWithoutExtension(executableName)))
+        {
+            processIdList.Add((uint)process.Id);
+        }
+        return processIdList;
+    }
+
+    private static string GetErrorText(eCaptureError error)
+    {
+        return error switch
+        {
+            eCaptureError.NONE => "Success",
+            eCaptureError.PARAM => "Invalid parameter",
+            eCaptureError.STATE => "Invalid operation for current state",
+            eCaptureError.NOT_AVAILABLE => "Feature not available",
+            eCaptureError.FORMAT => "CaptureFormat is invalid or not initialized",
+            eCaptureError.PROCESSID => "ProcessId is invalid (0/not set)",
+            eCaptureError.DEVICE => "Failed to get device",
+            eCaptureError.ACTIVATION => "Failed to activate device",
+            eCaptureError.INITIALIZE => "Failed to init device",
+            eCaptureError.SERVICE => "Failed to get interface pointer via service",
+            eCaptureError.START => "Failed to start capture",
+            eCaptureError.STOP => "Failed to stop capture",
+            eCaptureError.EVENT => "Failed to create and set event",
+            eCaptureError.INTERFACE => "Failed to call Windows interface function",
+            _ => "Unknown"
+        };
+    }
+
+    [DllImport("ole32.dll")]
+    private static extern int CoInitializeEx(IntPtr pvReserved, uint dwCoInit);
+
+    [DllImport("ole32.dll")]
+    private static extern void CoUninitialize();
+
+    private const uint COINIT_MULTITHREADED = 0x0;
+    private const uint COINIT_APARTMENTTHREADED = 0x2;
 }
